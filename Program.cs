ac710a0d69ac8cef759c36e8718f3f7f445f04dd
justgoth <129898920+justgoth@users.noreply.github.com>
2024-08-29 22:12:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using CellMachine.Logic;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace CellMachine;
+
+internal static class Program
+{
+    private static Cells? _cells;
+
+    private static void Main(string[] args)
+    {
+        RenderWindow window = new RenderWindow(new VideoMode(1920, 1200), "Cell Machine");
+        _cells = new Cells(window, 160, 120);
+        
+        window.SetActive();
+        window.Closed += new EventHandler(OnClose);
+
+        while (window.IsOpen)
+        {
+            window.Clear();
+            window.DispatchEvents();
+            _cells.Step();
+            _cells.Draw();
+            window.Display();
+        }
+    }
+
+    private static void OnClose(object? ssender, EventArgs e)
+    {
+        var window = (RenderWindow)ssender!;
+        window.Close();
+    }
+}
